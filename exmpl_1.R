defd478c40e61dc8b26c65218054dfd373c5e9c1
Dr K <wkitto1@avc.edu>
2019-08-25 10:59:58 -0700

    Add more examples

--- exmpl_1.R
+++ exmpl_1.R
@@ -1,3 +1,31 @@
 require(tidyverse)
 require(ggvis)
-ggvis(mtcars, props(~wt, ~mpg)) 
+mtcars %>% ggvis(~wt, ~mpg)%>% layer_points() %>%
+  add_axis("x", title = "Weight") %>%
+  add_axis("y", title = "Miles per gallon")
+
+mtcars %>% ggvis(~wt, ~mpg) %>%
+  layer_points() %>%
+  add_axis("x") %>%
+  add_axis("x", offset = 40, grid = FALSE)
+
+mtcars2 <- mtcars %>% dplyr::mutate(cyl = ordered(mtcars$cyl))
+mtcars2 %>% ggvis(~mpg, ~wt, size = ~cyl, fill = ~cyl) %>% layer_points()
+
+mtcars %>% 
+  ggvis(~wt, ~mpg, 
+        size := input_slider(10, 100),
+        opacity := input_slider(0, 1)
+  ) %>% 
+  layer_points()
+
+
+  
+mtcars %>% ggvis(~wt, ~mpg) %>%
+  layer_smooths(span = 1) %>%
+  layer_smooths(span = 0.3, stroke := "red")
+
+mtcars %>% 
+  ggvis(~wt) %>% 
+  layer_histograms(width =  input_slider(0, 2, step = 0.10, label = "width"),
+                   center = input_slider(0, 2, step = 0.05, label = "center"))
